b47b0b1947a5dda2d6450eae8346e45116b90371
Charles Parmentier <32079518+Cpavrai@users.noreply.github.com>
2022-06-15 15:32:25 +0200

    Scripts: Added scripts for lowercasing

---
+++ scripts/lowerfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# convert all files names in the current directory to lower case
+
+find . -type f -depth | while read x; do
+    lc=`echo "$x"  | tr '[A-Z]' '[a-z]'`
+    if [ "$lc" != "$x" ]; then
+      mv -v "$x" "$lc"
+    fi
+done
---
+++ scripts/lowerfolders.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# convert all folders names in the current directory to lower case
+
+find . -type d -depth | while read x; do
+    lc=`echo "$x"  | tr '[A-Z]' '[a-z]'`
+    if [ "$lc" != "$x" ]; then
+      mv -v "$x" "$lc"
+    fi
+done
