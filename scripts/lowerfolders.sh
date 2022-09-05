0e78f484095d476e3dbcaebd7c9c74466e83c507
Charles Parmentier <32079518+Cpavrai@users.noreply.github.com>
2022-09-05 15:27:09 +0200

    Folder Lowercase: Resolve issue with processing

--- scripts/lowerfolders.sh
+++ scripts/lowerfolders.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # convert all folders names in the current directory to lower case
 
-find . -type d | while read x; do
-    lc=`echo "$x" | tr '[A-Z]' '[a-z]'`
+find . -type d -depth | while read x; do
+    if [ "$x" = "." ]; then
+        continue
+    fi
+    lc_dirname=`echo "${x##*/}" | tr '[A-Z]' '[a-z]'`
+    lc=`echo "${x%/*}/${lc_dirname}"`
     if [ "$lc" != "$x" ]; then
         mv -v "$x" "$lc"
     fi
