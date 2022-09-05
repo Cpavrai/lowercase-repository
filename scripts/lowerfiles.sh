8b46a7fc9153ab5e01983e120bc0cb34cdf6ffaf
Charles Parmentier <32079518+Cpavrai@users.noreply.github.com>
2022-09-05 14:43:55 +0200

    Action: Updated script

--- scripts/lowerfiles.sh
+++ scripts/lowerfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # convert all files names in the current directory to lower case
 
-find . -type f -depth | while read x; do
-    lc=`echo "$x"  | tr '[A-Z]' '[a-z]'`
+find . -type f | while read x; do
+    lc=`echo "$x" | tr '[A-Z]' '[a-z]'`
     if [ "$lc" != "$x" ]; then
       mv -v "$x" "$lc"
     fi
--- scripts/lowerfolders.sh
+++ scripts/lowerfolders.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # convert all folders names in the current directory to lower case
 
-find . -type d -depth | while read x; do
-    lc=`echo "$x"  | tr '[A-Z]' '[a-z]'`
+find . -type d | while read x; do
+    lc=`echo "$x" | tr '[A-Z]' '[a-z]'`
     if [ "$lc" != "$x" ]; then
       mv -v "$x" "$lc"
     fi
